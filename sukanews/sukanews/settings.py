10adc1a16735ec9c7cb6746c05e4cac0301baa8d
Arbath Abdurrahman <arbath21@gmail.com>
2025-05-05 20:15:54 +0700

    Build Beranda

--- sukanews/sukanews/settings.py
+++ sukanews/sukanews/settings.py
@@ -113,8 +113,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+#untuk load gambar dari database yang tehubung dengan model
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- sukanews/sukanews/urls.py
+++ sukanews/sukanews/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from sukanews.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('kampus/',kampus, name='kampus'),
     path('event/',event, name='event'),
     path('info/',info, name='info'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
