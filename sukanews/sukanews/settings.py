af6dd5162155148f9463473108196da0646bc156
Arbath Abdurrahman <arbath21@gmail.com>
2025-06-15 18:29:18 +0700

    deploy tahap 1

--- sukanews/sukanews/settings.py
+++ sukanews/sukanews/settings.py
@@ -59,9 +59,9 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': config('DB_NAME', default='db_kalijagapost'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='admin123'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
     }
